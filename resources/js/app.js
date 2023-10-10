3fbcbedca02a24665d4a56233e77472f06abfb0f
“jessy <jessynesola@gmail.com>
2023-10-11 00:44:59 +0200

    cleaning the structure

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,72 +1,75 @@
-import 'bootstrap';
-import './bootstrap';
+import "bootstrap";
+import "./bootstrap";
 
-var sitePlusMinus = function() {
+var sitePlusMinus = function () {
+    var value,
+        quantity = document.getElementsByClassName("quantity-container");
 
-		var value,
-    		quantity = document.getElementsByClassName('quantity-container');
+    function createBindings(quantityContainer) {
+        var quantityAmount =
+            quantityContainer.getElementsByClassName("quantity-amount")[0];
+        var increase = quantityContainer.getElementsByClassName("increase")[0];
+        var decrease = quantityContainer.getElementsByClassName("decrease")[0];
+        increase.addEventListener("click", function (e) {
+            increaseValue(e, quantityAmount);
+        });
+        decrease.addEventListener("click", function (e) {
+            decreaseValue(e, quantityAmount);
+        });
+    }
 
-		function createBindings(quantityContainer) {
-	      var quantityAmount = quantityContainer.getElementsByClassName('quantity-amount')[0];
-	      var increase = quantityContainer.getElementsByClassName('increase')[0];
-	      var decrease = quantityContainer.getElementsByClassName('decrease')[0];
-	      increase.addEventListener('click', function (e) { increaseValue(e, quantityAmount); });
-	      decrease.addEventListener('click', function (e) { decreaseValue(e, quantityAmount); });
-	    }
+    function init() {
+        for (var i = 0; i < quantity.length; i++) {
+            createBindings(quantity[i]);
+        }
+    }
 
-	    function init() {
-	        for (var i = 0; i < quantity.length; i++ ) {
-						createBindings(quantity[i]);
-	        }
-	    };
+    function increaseValue(event, quantityAmount) {
+        value = parseInt(quantityAmount.value, 10);
 
-	    function increaseValue(event, quantityAmount) {
-	        value = parseInt(quantityAmount.value, 10);
+        console.log(quantityAmount, quantityAmount.value);
 
-	        console.log(quantityAmount, quantityAmount.value);
+        value = isNaN(value) ? 0 : value;
+        value++;
+        quantityAmount.value = value;
+    }
 
-	        value = isNaN(value) ? 0 : value;
-	        value++;
-	        quantityAmount.value = value;
-	    }
+    function decreaseValue(event, quantityAmount) {
+        value = parseInt(quantityAmount.value, 10);
 
-	    function decreaseValue(event, quantityAmount) {
-	        value = parseInt(quantityAmount.value, 10);
+        value = isNaN(value) ? 0 : value;
+        if (value > 0) value--;
 
-	        value = isNaN(value) ? 0 : value;
-	        if (value > 0) value--;
+        quantityAmount.value = value;
+    }
 
-	        quantityAmount.value = value;
-	    }
+    init();
+};
+sitePlusMinus();
 
-	    init();
+document.addEventListener("DOMContentLoaded", () => {
+    "use strict";
 
-	};
-	sitePlusMinus();
-
-    document.addEventListener('DOMContentLoaded', () => {
-        "use strict";
-
-    new Swiper('.slides-1', {
+    new Swiper(".slides-1", {
         speed: 600,
         loop: true,
         autoplay: {
-          delay: 5000,
-          disableOnInteraction: false
+            delay: 5000,
+            disableOnInteraction: false,
         },
-        slidesPerView: 'auto',
+        slidesPerView: "auto",
         pagination: {
-          el: '.swiper-pagination',
-          type: 'bullets',
-          clickable: true
+            el: ".swiper-pagination",
+            type: "bullets",
+            clickable: true,
         },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        }
-      });
-
-      window.addEventListener('load', () => {
-        aos_init();
-      });
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
     });
+
+    window.addEventListener("load", () => {
+        aos_init();
+    });
+});
