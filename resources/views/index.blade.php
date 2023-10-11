3bf1f47308a394596bb7d45b57409be4e797a4cd
“jessy <jessynesola@gmail.com>
2023-10-11 10:50:32 +0200

    add content on body homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,8 @@
 
     <x-header />
 
-    {{-- togli i commenti per vedere la homepage iniziale --}}
-    {{-- <x-body_index /> --}}
+    {{-- togli o rimetti il commento per vedere il contenuto iniziale --}}
+    <x-body_index />
 
     <x-footer />
 </x-layout>
