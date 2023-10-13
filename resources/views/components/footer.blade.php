aa37b8ebdcb4728ce93d9345ff8642d88d3706bd
Alessandro Mascellini <mascellinialessandro@gmail.com>
2023-10-13 16:35:17 +0200

    Modifiche Footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -33,53 +33,45 @@
 
         <div class="row g-5 mb-5">
             <div class="col-lg-4">
-                <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">CardBazaar<span>.</span></a>
+                <div class="mb-4 footer-logo-wrap text-center"><a href="#" class="footer-logo">CardBazaar<span>.</span></a>
                 </div>
-                <p class="mb-4">Cosa ci scriviamo qua? Cosa ci scriviamo qua? Cosa ci scriviamo qua? Cosa ci scriviamo qua? Cosa ci scriviamo qua? Cosa ci scriviamo qua? Cosa ci scriviamo qua?</p>
+                <p class="mb-4 text-center">Cosa ci scriviamo qua? Cosa ci scriviamo qua? Cosa ci scriviamo qua? Cosa ci scriviamo qua? Cosa ci scriviamo qua? Cosa ci scriviamo qua? Cosa ci scriviamo qua?</p>
 
-                <ul class="list-unstyled custom-social">
+                {{-- <ul class="list-unstyled custom-social">
                     <li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
                     <li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
                     <li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
                     <li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
-                </ul>
+                </ul> --}}
             </div>
 
             <div class="col-lg-8">
                 <div class="row links-wrap">
-                    <div class="col-6 col-sm-6 col-md-3">
+                    <div class="col-6 col-sm-6 col-md-6">
                         <ul class="list-unstyled">
-                            <li><a href="#">About us</a></li>
-                            <li><a href="#">Services</a></li>
+                            <li><a href="#"></a></li>
+                            {{-- <li><a href="#">Services</a></li>
                             <li><a href="#">Blog</a></li>
-                            <li><a href="#">Contact us</a></li>
+                            <li><a href="#">Contact us</a></li> --}}
                         </ul>
                     </div>
 
-                    <div class="col-6 col-sm-6 col-md-3">
-                        <ul class="list-unstyled">
-                            <li><a href="#">Support</a></li>
-                            <li><a href="#">Knowledge base</a></li>
-                            <li><a href="#">Live chat</a></li>
+                    <div class="col-6 col-sm-6 col-md-6">
+                        <ul class="">
+                            <li><a href="{{route('homepage')}}">Home</a></li>
+                            <li><a href="{{route('announcements.index')}}">Explore</a></li>
+                            <li><a href="{{route('categories.index')}}">Categories</a></li>
                         </ul>
                     </div>
 
-                    <div class="col-6 col-sm-6 col-md-3">
+                    {{-- <div class="col-6 col-sm-6 col-md-4">
                         <ul class="list-unstyled">
                             <li><a href="#">Jobs</a></li>
                             <li><a href="#">Our team</a></li>
                             <li><a href="#">Leadership</a></li>
                             <li><a href="#">Privacy Policy</a></li>
                         </ul>
-                    </div>
-
-                    <div class="col-6 col-sm-6 col-md-3">
-                        <ul class="list-unstyled">
-                            <li><a href="#">Nordic Chair</a></li>
-                            <li><a href="#">Kruzo Aero</a></li>
-                            <li><a href="#">Ergonomic Chair</a></li>
-                        </ul>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
 
