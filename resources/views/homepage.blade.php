82f1dc724012e1f4029fb73fcbb7b18f57c9697c
“jessy <jessynesola@gmail.com>
2023-10-15 17:34:04 +0200

    mods on homepage 1

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,6 +2,41 @@
     <x-header />
 <body>
     <main>
+
+      <div class="product-section">
+        <div class="container">
+            <div class="row">
+
+                <!-- Start Column 1 -->
+                
+                <div class="col-md-12 col-lg-3 my-5 mb-lg-0">
+                    <h2 class="mb-4 section-title">Ecco le ultime aggiunte da altri utenti!</h2>
+                    <p class="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
+                        vulputate velit imperdiet dolor tempor tristique. </p>
+                    <p><a href="{{route('announcements.index')}}" class="btn">Mostra altro</a></p>
+                </div>
+                </article>
+                <!-- End Column 1 -->
+
+                <!-- Start Column 2 -->
+                @foreach ($announcements as $announcement)
+                    <div class="col-12 col-md-4 col-lg-3 my-5 mb-md-0">
+                        <div class="product-item">
+                            <img src="assets/images/yugioh (1).jpg" class="img-fluid product-thumbnail">
+                            <h3 class="product-title">{{ $announcement->title }}</h3>
+                            <strong class="product-price">{{ $announcement->price }}€</strong>
+
+                            <span class="icon-cross">
+                                <a href="{{route('announcements.show', $announcement->uri)}}"><img src="assets/images/cross.svg" class="img-fluid"></a>
+                            </span>
+                        </div>
+                    </div>
+                @endforeach
+                <!-- End Column 2 -->
+            </div>
+        </div>
+    </div>
+
       <article>
         <img src="1.png" />
         <div>
