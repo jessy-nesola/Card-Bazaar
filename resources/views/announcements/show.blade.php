110361761af1f8a8f2b091722a8e3988abb00c50
Emanuele Pasquale Papale <emanuelep.papale@gmail.com>
2023-10-17 19:38:08 +0200

    test su carosello show announcements

--- resources/views/announcements/show.blade.php
+++ resources/views/announcements/show.blade.php
@@ -6,27 +6,43 @@
             <div class="container">
                 <div class="row">
 
-                    <div id="carouselExampleAutoplaying" class="col-lg-6 d-flex h-100 carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-                          <div class="carousel-item active justify-content-center">
-                            <img src="{{asset('assets/images/card-1.avif')}}" class="d-block w-100" alt="...">
+
+                        <div id="carousel" class="carousel col-6 d-flex">
+
+                            <div id='item_1' class="hideLeft">
+                              <img src="https://cdn.pixabay.com/photo/2017/08/15/08/23/stars-2643089__340.jpg">
+                            </div>
+                            <div id='item_2' class="prevLeftSecond">
+                              <img src="https://cdn.pixabay.com/photo/2012/11/28/11/28/rocket-launch-67723__340.jpg">
+                            </div>
+                            <div id='item_3' class="prev">
+                              <img src="https://cdn.pixabay.com/photo/2018/08/15/13/10/galaxy-3608029_960_720.jpg">
+                            </div>
+                            <div id='item_4' class="selected">
+                              <img src="https://cdn.pixabay.com/photo/2020/06/17/09/28/wormhole-5308810__340.jpg">
+                            </div>
+                            <div id='item_5' class="next">
+                              <img src="https://cdn.pixabay.com/photo/2016/11/18/22/58/stars-1837306__340.jpg">
+                            </div>
+                            <div id='item_6' class="nextRightSecond">
+                              <img src="https://cdn.pixabay.com/photo/2017/02/09/09/11/starry-sky-2051448__340.jpg">
+                            </div>
+                            <div id='item_7' class="hideRight">
+                              <img src="https://cdn.pixabay.com/photo/2011/12/15/11/37/galaxy-11188__340.jpg">
+                            </div>
+                            <div id='item_8' class="hideRight">
+                              <img src="https://cdn.pixabay.com/photo/2011/12/15/11/32/pismis-24-11186__340.jpg">
+                            </div>
                           </div>
-                          <div class="carousel-item justify-content-center">
-                            <img src="{{asset('assets/images/card-2.avif')}}" class="d-block w-100" alt="...">
+
+                          <div class="buttons">
+                            <button class="button-82-pushable" role="button" id="prev"><span class="button-82-shadow"></span><span
+                                class="button-82-edge"></span><span class="button-82-front text">Previous</span></button>
+
+
+                            <button class="button-82-pushable" role="button" id="next"><span class="button-82-shadow"></span><span
+                                class="button-82-edge"></span><span class="button-82-front text">Next</span></button>
                           </div>
-                          <div class="carousel-item justify-content-center">
-                            <img src="{{asset('assets/images/card-3.avif')}}" class="d-block w-100" alt="...">
-                          </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Next</span>
-                        </button>
-                      </div>
 
                     <div class="col-md-12 col-lg-3 mb-lg-0 col-lg-6 d-flex justify-content-center">
                         <div class="card-description mx-5">
@@ -40,7 +56,7 @@
                             <div><strong>Descrizione:</strong></div>
                             <p class="mb-4">{{ $announcement->body }}</p>
                             <h5><strong>{{ $announcement->price }}€</strong></h5>
-                            <p><a href="{{route('register')}}" class="btn btn-warning me-2">Contatta il venditore</a>
+                            <p><a href="{{ route('register') }}" class="btn btn-warning me-2">Contatta il venditore</a>
                         </div>
                     </div>
                 </div>
@@ -60,4 +76,5 @@
                     </div>
                 </div> --}}
     </div>
+
 </x-layout>
