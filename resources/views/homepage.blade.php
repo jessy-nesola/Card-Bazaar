8676b568594980bd349a889f751381315f062e65
PierluigiPerrino <pierluigi.perrino01@outlook.it>
2023-10-18 21:42:57 +0200

    modifiche homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,59 +7,51 @@
         </div>
     @endif
     @if (session()->has('send.ok'))
-    <div>
-        <div class="text-center mt-5">
-            <h3 class="text-decoration-underline">{{ session()->get('send.ok') }}</h3>
+        <div>
+            <div class="text-center mt-5">
+                <h3 class="text-decoration-underline">{{ session()->get('send.ok') }}</h3>
+            </div>
         </div>
-    </div>
-@endif
+    @endif
     <x-header />
-
-    <div class="container col-xxl-8 px-4 py-5">
-        <hr class="my-4"/>
-        <div class="row gx-4 gx-lg-5 justify-content-center">
-            <div class="col-md-10 col-lg-8 col-xl-7">
+    <div class="product-section">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
                 <div>
-                    <a>
-                        <h2 class="post-title">Ecco le ultime carte aggiunte da altri utenti!</h2>
-                        <h3 class="post-subtitle">Che testo mettiamo?</h3>
-                    </a>
+                    <h2 class="text-center mt-5">Ecco gli ultimi annunci degli utenti!</h2>
                 </div>
+                @foreach ($announcements as $announcement)
+                    <div class="col-12 col-md-4 col-lg-3 my-5 mb-md-0">
+                        <div class="custom-card">
+                            <div class="custom-card-content shadow">
+                                <img src="assets/images/yugioh (1).jpg" class="img-fluid product-thumbnail">
+                                <h3 class="name">{{ $announcement->title }}</h3>
+                                <strong class="product-price">{{ $announcement->price }}€</strong>
+                                <a href="{{ route('announcements.show', $announcement->uri) }}"
+                                    class="btn btn-primary">Visualizza</a>
 
-                {{-- buttaci dentro le carte --}}
-                <div class="container">
-                    <div class="row">
-                        @foreach ($announcements as $announcement)
-                            <div class="col-12 col-md-4 col-lg-3 mx- my-5 mb-md-0">
-                                <div class="item">
-                                    <img src="assets/images/yugioh (1).jpg" class="img-fluid product-thumbnail">
-                                    <h5 class="">{{ $announcement->title }}</h5>
-                                    <p class="price">{{ $announcement->price }}€</p>
-        
-                                    <span class="icon-cross">
-                                        <a href="{{ route('announcements.show', $announcement->uri) }}"><img
-                                                src="assets/images/cross.svg" class="img-fluid"></a>
-                                    </span>
-                                </div>
                             </div>
-                        @endforeach
+                        </div>
                     </div>
-                </div>
+                @endforeach
+                <div class="mb-5"></div>
+                {{-- MENU NAVIGAZIONE --}}
             </div>
         </div>
     </div>
 
-
-    {{-- sezione da testare  --}}
     <div class="container col-xxl-8 px-4 py-5">
-        <hr class="my-4"/>
+        <hr class="my-4" />
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
             <div class="col-lg-6">
-                {{-- <h1 class="display-5 fw-bold lh-1 mb-3">Quale titolo?</h1> --}}
-                <p class="lead">É il tuo bazar online per appassionati di carte da gioco. Scopri una vasta selezione di carte da collezione, mazzi personalizzati e accessori per migliorare la tua esperienza di gioco. Trova le carte più rare e cerca tra le offerte esclusive. Soddisfa la tua sete di strategia e competizione con CardBazaar.</p>
+                <h1 class="display-5 fw-bold lh-1 mb-3">Quale titolo?</h1>
+                <p class="lead">É il tuo bazar online per appassionati di carte da gioco. Scopri una vasta selezione
+                    di carte da collezione, mazzi personalizzati e accessori per migliorare la tua esperienza di gioco.
+                    Trova le carte più rare e cerca tra le offerte esclusive. Soddisfa la tua sete di strategia e
+                    competizione con CardBazaar.</p>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    {{-- <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button> --}}
+                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
                 </div>
             </div>
             <div class="col-10 col-sm-8 col-lg-6">
@@ -67,8 +59,28 @@
                     width="600" height="500" loading="lazy">
             </div>
         </div>
-        <hr class="my-4"/>
+        <hr class="my-4" />
     </div>
+
+    {{-- sezione da testare  --}}
+    {{-- <div class="container col-xxl-8 px-4 py-5">
+        <hr class="my-4"/>
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-lg-6"> --}}
+    {{-- <h1 class="display-5 fw-bold lh-1 mb-3">Quale titolo?</h1> --}}
+    {{-- <p class="lead">É il tuo bazar online per appassionati di carte da gioco. Scopri una vasta selezione di carte da collezione, mazzi personalizzati e accessori per migliorare la tua esperienza di gioco. Trova le carte più rare e cerca tra le offerte esclusive. Soddisfa la tua sete di strategia e competizione con CardBazaar.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start"> --}}
+    {{-- <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button> --}}
+    {{-- </div>
+            </div>
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="/assets/images/mix_cards.jpg" class="d-block mx-lg-auto img-fluid" alt="Mixed Cards"
+                    width="600" height="500" loading="lazy">
+            </div>
+        </div>
+        <hr class="my-4"/>
+    </div> --}}
 
     {{-- VECCHIA SEZIONE CON LE CARTE --}}
     {{-- <div class="section">
