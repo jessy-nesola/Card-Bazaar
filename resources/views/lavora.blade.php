44f629ff2d4394cdde91e0cb527aaae5fabf7c5e
PierluigiPerrino <pierluigi.perrino01@outlook.it>
2023-10-18 19:28:44 +0200

    Modifica pagina lavora con noi

--- resources/views/lavora.blade.php
+++ resources/views/lavora.blade.php
@@ -1,11 +1,10 @@
 <x-layout>
-    <div class="container px-5">
+    <div class="container px-5 mt-4">
         <div class="row gx-5 align-items-center justify-content-center">
             <div class="col-lg-8 col-xl-7 col-xxl-6">
                 <div class="my-5 text-center text-xl-start text-light">
                     <h2 class="animate__animated animate__heartBeat mb-3">Siamo felici di vederti qui!</h2>
-                    <p class="mb-4 ">Raccontaci di te, delle tue esperienze e delle tue passioni tramite il form qui
-                        sotto.</p>
+                    <p class="mb-4 ">Al momento siamo alla ricerca di Revisori per il nostro sito, per candidarti e lavorare con noi compila il form qui sotto.</p>
                 </div>
             </div>
 
@@ -19,7 +18,7 @@
             <div class="row justify-content-center">
                 <div class="col-md-6">
                     <div class="">
-                        <h2 class="mb-4 text-center">Che scriviamo?</h2>
+                        <h2 class="mb-4 text-center">Raccontaci di te!</h2>
                         <form action="{{route('become.revisor')}}" method="POST">
                             @csrf
                             @method('POST')
