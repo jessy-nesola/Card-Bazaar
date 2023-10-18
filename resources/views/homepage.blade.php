1d03ad8ae35def18352aff86a76f6ad8c9fe8e2d
“jessy <jessynesola@gmail.com>
2023-10-18 20:49:42 +0200

    partial mods on homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -15,8 +15,44 @@
 @endif
     <x-header />
 
+    <div class="container col-xxl-8 px-4 py-5">
+        <hr class="my-4"/>
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <div>
+                    <a>
+                        <h2 class="post-title">Ecco le ultime carte aggiunte da altri utenti!</h2>
+                        <h3 class="post-subtitle">Che testo mettiamo?</h3>
+                    </a>
+                </div>
+
+                {{-- buttaci dentro le carte --}}
+                <div class="container">
+                    <div class="row">
+                        @foreach ($announcements as $announcement)
+                            <div class="col-12 col-md-4 col-lg-3 mx- my-5 mb-md-0">
+                                <div class="item">
+                                    <img src="assets/images/yugioh (1).jpg" class="img-fluid product-thumbnail">
+                                    <h5 class="">{{ $announcement->title }}</h5>
+                                    <p class="price">{{ $announcement->price }}€</p>
+        
+                                    <span class="icon-cross">
+                                        <a href="{{ route('announcements.show', $announcement->uri) }}"><img
+                                                src="assets/images/cross.svg" class="img-fluid"></a>
+                                    </span>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     {{-- sezione da testare  --}}
     <div class="container col-xxl-8 px-4 py-5">
+        <hr class="my-4"/>
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
             <div class="col-lg-6">
                 {{-- <h1 class="display-5 fw-bold lh-1 mb-3">Quale titolo?</h1> --}}
@@ -31,25 +67,19 @@
                     width="600" height="500" loading="lazy">
             </div>
         </div>
+        <hr class="my-4"/>
     </div>
 
-    <div class="product-section">
+    {{-- VECCHIA SEZIONE CON LE CARTE --}}
+    {{-- <div class="section">
         <div class="container">
             <div class="row">
-
-                <div class="col-md-12 col-lg-3 my-5 mb-lg-0 shadow">
-                    <h3 class="mb-4 section-title">Ecco le ultime carte aggiunte da altri utenti!</h3>
-                    <p class="mb-4">Che testo mettiamo? Che testo mettiamo? Che testo mettiamo? Che testo mettiamo?
-                    </p>
-                    <p><a href="{{ route('announcements.index') }}" class="btn btn-primary">Esplora</a></p>
-                </div>
-
                 @foreach ($announcements as $announcement)
                     <div class="col-12 col-md-4 col-lg-3 mx- my-5 mb-md-0">
-                        <div class="product-item">
+                        <div class="item">
                             <img src="assets/images/yugioh (1).jpg" class="img-fluid product-thumbnail">
-                            <h3 class="product-title">{{ $announcement->title }}</h3>
-                            <strong class="product-price">{{ $announcement->price }}€</strong>
+                            <h5 class="">{{ $announcement->title }}</h5>
+                            <p class="price">{{ $announcement->price }}€</p>
 
                             <span class="icon-cross">
                                 <a href="{{ route('announcements.show', $announcement->uri) }}"><img
@@ -58,7 +88,7 @@
                         </div>
                     </div>
                 @endforeach
-                {{-- <div class="wrapper container">
+                <div class="wrapper container">
                         <div class="cardHome text-center col-1">
                             <div class="image"> <img src="assets/images/yugioh (3).jpg" width="220"> </div>
                             <div class="pr-3 about-product">
@@ -66,9 +96,9 @@
                                 <h4>{{ $announcement->price }}</h4> <button href="{{ route('announcements.show', $announcement->uri) }}" class="btn btn-primary buy-now">Prova</button>
                             </div>
                         </div>
-                    </div> --}}
-                <!-- End Column 2 -->
+                    </div>
             </div>
         </div>
-    </div>
+    </div> --}}
+
 </x-layout>
