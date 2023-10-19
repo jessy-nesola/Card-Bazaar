61c8159dfdb1651cc9e48e1ed398904a92a54132
Alessandro Mascellini <mascellinialessandro@gmail.com>
2023-10-19 19:38:25 +0200

    Modifiche navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,14 +7,14 @@
         </button>
         <div class="navbar-collapse collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item @if (Route::currentRouteName() == 'homepage') border-bottom border-2 @endif">
+                <li class="nav-item @if (Route::currentRouteName() == 'homepage') @endif">
                     <a class="nav-link" aria-current="page" href="{{ route('homepage') }}">Home</a>
                 </li>
-                <li class="nav-item @if (Route::currentRouteName() == 'announcements.index') border-bottom border-2 @endif">
+                <li class="nav-item @if (Route::currentRouteName() == 'announcements.index') @endif">
                     <a class="nav-link" href="{{ route('announcements.index') }}">Esplora</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle @if (Route::currentRouteName() == 'categories.index') border-bottom border-2 @endif"
+                    <a class="nav-link dropdown-toggle @if (Route::currentRouteName() == 'categories.index') @endif"
                         href="#" id="navbarDropdown" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
                         Categorie
@@ -31,7 +31,7 @@
                 </li>
                 @auth
                 <li class="nav-item">
-                    <a class="nav-link @if (Route::currentRouteName() == 'announcements.create') border-bottom border-2 @endif" href="{{ route('announcements.create') }}" tabindex="-1"
+                    <a class="nav-link @if (Route::currentRouteName() == 'announcements.create') @endif" href="{{ route('announcements.create') }}" tabindex="-1"
                         aria-disabled="true">Inserisci annuncio</a>
                 </li>
                 @endauth
