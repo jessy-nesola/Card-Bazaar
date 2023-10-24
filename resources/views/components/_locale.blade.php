08958a23eb9b6907d70d94063875fe10cc90fef3
“jessy <jessynesola@gmail.com>
2023-10-24 13:03:36 +0200

    changes on footer

--- resources/views/components/_locale.blade.php
+++ resources/views/components/_locale.blade.php
@@ -1,7 +1,7 @@
 <form action="{{ route ('set_language', $lang) }}" method="POST">
     @csrf
     <li class="text-white-50" type="submit">
-        <button type="submit">
+        <button class="btn btn-warning btn-sm mb-2" type="submit">
             <span>{{ $nation }}</span>
         </button>
     </li>
