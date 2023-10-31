ce13aa4b01f6fa0ac28e1b98ada63c09a1413caf
Alessandro Mascellini <mascellinialessandro@gmail.com>
2023-10-31 14:35:58 +0100

    Sistemati margini ann.index & cat.show

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -1,35 +1,23 @@
 <x-layout>
-    {{-- <div class="hero" style="margin-top: 70px;">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-5">
-                    <div class="intro-excerpt">
-                        <h2 class="animate__animated animate__pulse fs-1">Esplora da qui tutte le carte!</h2>
-                        <p class="animate__animated animate__pulse">Che testo mettiamo? Che testo mettiamo? Che testo
-                            mettiamo? Che testo mettiamo?</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
-
-    <div class="intro-excerpt text-center" style="margin-top: 4%">
-        <h2 class="animate__animated animate__pulse fs-1">{{__('custom.ann.ind1')}}</h2>
-        <p class="animate__animated animate__pulse">{{__('custom.ann.ind2')}}</p>
+    <div class="intro-excerpt text-center mt-5">
+        <h2 class="animate__animated animate__pulse fs-1">{{ __('custom.ann.ind1') }}</h2>
+        <p class="animate__animated animate__pulse">{{ __('custom.ann.ind2') }}</p>
     </div>
 
     <div class="product-section">
         <div class="container">
             <div class="row">
                 @forelse ($announcements as $announcement)
-                <x-card :target='$announcement'/>
+                    <x-card :target='$announcement' />
                 @empty
-                <h3 class="mb-3">{{__('custom.ann.ind3')}}</h3>
-                <div><a href="{{ route('announcements.create') }}" class="btn btn-secondary btn-sm">{{__('custom.ann.ind4')}}</a></div>
+                    <h3 class="my-3">{{ __('custom.ann.ind3') }}</h3>
+                    <div><a href="{{ route('announcements.create') }}"
+                            class="btn btn-secondary btn-sm">{{ __('custom.ann.ind4') }}</a></div>
                 @endforelse
-                <div class="mb-5"></div>
-                {{-- MENU NAVIGAZIONE --}}
-                {{ $announcements->links() }}
+                <div class="mt-2 mb-5">
+                    {{-- MENUNAVIGAZIONE --}}
+                    {{ $announcements->links() }}
+                </div>
             </div>
         </div>
     </div>
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,19 +1,5 @@
 <x-layout>
-    {{-- <header class="hero" style="margin-top: 130px;">
-        <div class="container">
-            <div class="row justify-content-between">
-                <div class="col-lg-5">
-                    <div class="intro-excerpt">
-                        <h2 class="fs-1">{{ $category->name }}</h2>
-                        <p class="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam
-                            vulputate velit imperdiet dolor tempor tristique.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header> --}}
-
-    <div class="intro-excerpt text-center" style="margin-top: 4%">
+    <div class="intro-excerpt text-center mt-5">
         <h2 class="animate__animated animate__pulse fs-1">{{ $category->name }}</h2>
         {{-- <p class="animate__animated animate__pulse">{{__('custom.cat.show1')}} {{ $category->name }}</p> --}}
     </div>
@@ -25,14 +11,14 @@
                 <x-card :target='$announcement'/>
                 @empty
                 <div class="text-center">
-                    <h3 class="mt-5 mb-3">{{__('custom.cat.show2')}}</h3>
+                    <h3 class="my-3">{{__('custom.cat.show2')}}</h3>
                     <div><a href="{{ route('announcements.create') }}" class="btn btn-warning btn-sm">{{__('custom.cat.show3')}}</a></div>
                 </div>
                 @endforelse
-                <span class="mt-4">
+                <div class="mt-2 mb-5">
                     {{-- MENU NAVIGAZIONE --}}
                     {{ $category->orderByAndPaginate()->links() }}
-                </span>
+                </div>
             </div>
         </div>
     </div>
