a6de82d003cb35c1fd0f9df81524cf15b97f44dd
EmanuelePasquale <emanuelep.papale@gmail.com>
2023-10-31 23:33:03 +0100

    ragazzi mi sono preso la lobertà di modificare la view della mail. siate clementi

--- resources/views/mail/become_revisor.blade.php
+++ resources/views/mail/become_revisor.blade.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <style>
         body {
             background-color: #fff;
             padding: 0;
             margin: 0;
+            background-image: url('../../../public/assets/images/background.png');
+            background-size: cover;
+            background-repeat: repeat;
+
         }
+
         .header {
             width: 100%;
             display: flex;
@@ -18,14 +24,17 @@
             height: 60px;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         }
+
         h3 {
             color: #FFC107;
             font-size: 28px;
         }
+
         .content {
             height: 400px;
             width: 70%;
             margin-top: 30px;
+            margin-bottom: 30px;
             margin-left: 15%;
             border-radius: 10px;
             display: flex;
@@ -33,6 +42,7 @@
             align-items: start;
             background-color: rgb(201, 195, 195);
         }
+
         .info {
             display: flex;
             justify-content: flex-start;
@@ -40,21 +50,39 @@
             align-items: center;
             margin-bottom: 10px;
         }
+
         .label {
-            color:blueviolet;
-            font-size: 22px;
+            color: #5933b1;
+            font-size: 20px;
         }
+
         .value {
             color: #000;
             font-size: 18px;
         }
-        .make-revisor-link {
-            color: #000;
+
+        .make-revisor-button {
+            background-color: #5933b1;
+            color: #fff;
             font-size: 18px;
-            margin-bottom: 10px;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .make-revisor-button a {
+            text-decoration: none;
+        }
+        .footer {
+            background-color: #5933b1;
+            color: #fff;
+            text-align: center;
+            padding: 10px;
         }
     </style>
 </head>
+
 <body>
     <div class="header">
         <h3>CardBazaar</h3>
@@ -62,25 +90,29 @@
 
     <div class="content">
         <div class="info">
-            <p class="label"><strong>Mail inviata da:</strong></p>
+            <p class="label"><strong>Mail inviata da: </strong></p>
             <p class="value">{{$user->name}}</p>
         </div>
         <div class="info">
-            <p class="label"><strong>eMail:</strong></p>
+            <p class="label"><strong>eMail: </strong></p>
             <p class="value">{{$user->email}}</p>
         </div>
         <div class="info">
-            <p class="label"><strong>Utente registrato il:</strong></p>
+            <p class="label"><strong>Utente registrato il: </strong></p>
             <p class="value">{{$user->created_at->format('d/m/Y')}}</p>
         </div>
         <div class="info">
-            <p class="label"><strong>Messaggio:</strong></p>
+            <p class="label"><strong>Messaggio: </strong></p>
             <p class="value">{{$content['aboutYou']}}</p>
         </div>
         <div class="info">
-            <p class="value">Se vuoi renderlo revisore clicca qui: <a class="make-revisor-link" href="{{route('make.revisor', compact('user'))}}">Rendi revisore.</a></p>
+            <button class="make-revisor-button"><a href="{{route('make.revisor', compact('user'))}}"
+                    style="color: #FFC107">Rendi revisore</a></button>
         </div>
     </div>
+        <div class="footer">
+            <p>Copyright &copy; Designed by DevelHopers, 2023</p>
+          </div>
 </body>
-</html>
 
+</html>
