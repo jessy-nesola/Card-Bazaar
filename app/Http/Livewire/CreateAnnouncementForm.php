72f95817c9c5225615613ab94b3dac8a0eb82099
Alessandro Mascellini <mascellinialessandro@gmail.com>
2023-11-01 15:35:03 +0100

    User Story 8 Completata

--- app/Http/Livewire/CreateAnnouncementForm.php
+++ app/Http/Livewire/CreateAnnouncementForm.php
@@ -12,6 +12,7 @@ use Livewire\WithFileUploads;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Config;
+use App\Jobs\RemoveFaces;
 
 class CreateAnnouncementForm extends Component
 {
@@ -76,14 +77,15 @@ class CreateAnnouncementForm extends Component
                 $newFileName = "announcements/{$this->announcement->id}";
                 $newImage = $this->announcement->images()->create(['path'=>$image->store($newFileName, 'public')]);
 
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
-                dispatch(new ResizeImage($newImage->path, 200, 300));
+                // dispatch(new GoogleVisionSafeSearch($newImage->id));
+                // dispatch(new GoogleVisionLabelImage($newImage->id));
+                // dispatch(new ResizeImage($newImage->path, 200, 300));
 
-                // RemoveFaces::withChain([
-                //     new ResizeImage($newImage->path, 200, 300),
-                //     new GoogleVisionSafeSearch($newImage->id)
-                // ])->dispatch($newImage->id);
+                RemoveFaces::withChain([
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id),
+                    new ResizeImage($newImage->path, 200, 300)
+                ])->dispatch($newImage->id);
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
