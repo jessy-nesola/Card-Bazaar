2f86b70d903935df26ce4a61333b09f6d283d526
“jessy <jessynesola@gmail.com>
2023-11-05 16:17:26 +0100

    changes on about us page

--- resources/views/about_us2.blade.php
+++ resources/views/about_us2.blade.php
@@ -5,8 +5,20 @@
     <div class="intro-excerpt text-center mt-5">
         <h2 class="animate__animated animate__pulse fs-1">Su CardBaazar</h2>
         <div class="container">
-            <div class="row">   
-                <div class="col-12 my-5 fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ex officia accusamus quae. Impedit rerum voluptate quidem voluptas labore amet modi sed ipsum! Sint ullam impedit excepturi illo itaque minima. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla libero maxime ipsa quae quisquam quibusdam illum! Iste sapiente perspiciatis incidunt eveniet aspernatur perferendis molestias exercitationem, at enim deleniti tempore ut! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat laudantium expedita explicabo facilis, iure fuga beatae ut, similique laboriosam rem, tempora praesentium deleniti consequuntur corporis? Exercitationem magnam quod aperiam at. Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus fugiat excepturi nemo vitae earum reiciendis numquam aliquam minus ut inventore? Eius vel consequatur, laborum quisquam eaque repellat nesciunt eum eveniet.</div>
+            <div class="row">
+                <p>La Nostra Missione è semplice: vogliamo mettere a disposizione degli
+                    appassionati di carte da gioco uno spazio online dedicato a una vasta gamma di giochi e carte
+                    collezionabili. Siamo impegnati a fornire un ambiente sicuro, affidabile e amichevole per
+                    l'acquisto, la vendita e lo scambio di carte, consentendo agli appassionati di connettersi, scoprire
+                    nuovi tesori e condividere le proprie collezioni.</p>
+                <p>Offriamo il luogo ideale per scoprire carte da gioco rare, ampliare la propria collezione, o
+                    vendere le proprie carte inutilizzate. Offriamo una vasta selezione di carte da gioco, dai classici
+                    alle ultime edizioni. La nostra piattaforma è progettata per semplificare il processo di ricerca e
+                    scambio, consentendo agli utenti di trovare esattamente ciò di cui hanno bisogno in modo intuitivo e
+                    veloce.</p>
+                <p>Siamo anche una comunità. Siamo orgogliosi di offrire supporto e
+                    risorse agli appassionati di carte da gioco, con forum e spazi dedicati alla discussione, alla
+                    condivisione di strategie e alla scoperta delle ultime novità del settore.</p>
             </div>
         </div>
         {{-- <p class="animate__animated animate__pulse">{{ __('custom.ann.ind2') }}</p> --}}
@@ -21,29 +33,31 @@
     <div class="product-section">
         <div class="container">
             <div class="row my-5">
-                <div class="col-12 col-lg-3">
-                    <div class="rounded-circle" style="border: 5px solid #FFC107">
-                        <img class="img-fluid rounded-circle" style="border: 5px solid rgb(66, 33, 104)" src="assets/images/staff/Alessandro-Mascellini.jpg" alt="">
+                <div class="col-12 col-lg-3 d-flex align-items-center flex-column">
+                    <img class="object-fit-cover rounded-circle"
+                        style="border: 5px solid rgb(66, 33, 104); width: 200px; height: 200px;"
+                        src="assets/images/staff/Alessandro-Mascellini.jpg" alt="">
+                    <div class="mb-5 mt-2 fs-4 fw-bold">Alessandro Mascellini <i class="fa-brands fa-linkedin"></i>
                     </div>
-                    <div class="text-center mb-5 mt-2 fs-4 fw-bold text-decoration-underline">Alessandro Mascellini</div>
                 </div>
-                <div class="col-12 col-lg-3">
-                    <div class="rounded-circle" style="border: 5px solid #FFC107">
-                        <img class="img-fluid rounded-circle" style="border: 5px solid rgb(66, 33, 104)" src="assets/images/staff/Alessandro-Mascellini.jpg" alt="">
-                    </div>
-                    <div class="text-center mb-5 mt-2 fs-4 fw-bold text-decoration-underline">Alessandro Mascellini</div>
+                <div class="col-12 col-lg-3 d-flex align-items-center flex-column">
+                    <img class="object-fit-cover rounded-circle"
+                        style="border: 5px solid rgb(66, 33, 104); width: 200px; height: 200px;"
+                        src="assets/images/staff/Jessy-Nesola.jpg" alt="">
+                    <div class="mb-5 mt-2 fs-4 fw-bold">Jessy Nesola <i class="fa-brands fa-linkedin"></i></div>
                 </div>
-                <div class="col-12 col-lg-3">
-                    <div class="rounded-circle" style="border: 5px solid #FFC107">
-                        <img class="img-fluid rounded-circle" style="border: 5px solid rgb(66, 33, 104)" src="assets/images/staff/Alessandro-Mascellini.jpg" alt="">
+                <div class="col-12 col-lg-3 d-flex align-items-center flex-column">
+                    <img class="object-fit-cover rounded-circle"
+                        style="border: 5px solid rgb(66, 33, 104); width: 200px; height: 200px;"
+                        src="assets/images/staff/Alessandro-Mascellini.jpg" alt="">
+                    <div class="mb-5 mt-2 fs-4 fw-bold">Alessandro Mascellini <i class="fa-brands fa-linkedin"></i>
                     </div>
-                    <div class="text-center mb-5 mt-2 fs-4 fw-bold text-decoration-underline">Alessandro Mascellini</div>
                 </div>
-                <div class="col-12 col-lg-3">
-                    <div class="rounded-circle" style="border: 5px solid #FFC107">
-                        <img class="img-fluid rounded-circle" style="border: 5px solid rgb(66, 33, 104)" src="assets/images/staff/Alessandro-Mascellini.jpg" alt="">
-                    </div>
-                    <div class="text-center mb-5 mt-2 fs-4 fw-bold text-decoration-underline">Alessandro Mascellini</div>
+                <div class="col-12 col-lg-3 d-flex align-items-center flex-column">
+                    <img class="object-fit-cover rounded-circle"
+                        style="border: 5px solid rgb(66, 33, 104); width: 200px; height: 200px;"
+                        src="assets/images/staff/Jessy-Nesola.jpg" alt="">
+                    <div class="mb-5 mt-2 fs-4 fw-bold">Jessy Nesola <i class="fa-brands fa-linkedin"></i></div>
                 </div>
             </div>
         </div>
