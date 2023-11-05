658b58cfdc602077358c656ad56403a7634b0da1
PierluigiPerrino <pierluigi.perrino01@outlook.it>
2023-11-05 16:38:30 +0100

    modifiche pagina chi siamo

--- resources/views/about_us2.blade.php
+++ resources/views/about_us2.blade.php
@@ -37,27 +37,27 @@
                     <img class="object-fit-cover rounded-circle"
                         style="border: 5px solid rgb(66, 33, 104); width: 200px; height: 200px;"
                         src="assets/images/staff/Alessandro-Mascellini.jpg" alt="">
-                    <div class="mb-5 mt-2 fs-4 fw-bold">Alessandro Mascellini <i class="fa-brands fa-linkedin"></i>
+                    <div class="mb-5 mt-2 fs-4 fw-bold">Alessandro Mascellini <a href="https://www.linkedin.com/in/alessandro-mascellini-full-stack-developer-junior/"><i class="fa-brands fa-linkedin"></i></a>
                     </div>
                 </div>
                 <div class="col-12 col-lg-3 d-flex align-items-center flex-column">
                     <img class="object-fit-cover rounded-circle"
                         style="border: 5px solid rgb(66, 33, 104); width: 200px; height: 200px;"
                         src="assets/images/staff/Jessy-Nesola.jpg" alt="">
-                    <div class="mb-5 mt-2 fs-4 fw-bold">Jessy Nesola <i class="fa-brands fa-linkedin"></i></div>
+                    <div class="mb-5 mt-2 fs-4 fw-bold">Jessy Nesola <a href="https://www.linkedin.com/in/jessynesola-dev/"><i class="fa-brands fa-linkedin"></i></a></div>
                 </div>
                 <div class="col-12 col-lg-3 d-flex align-items-center flex-column">
                     <img class="object-fit-cover rounded-circle"
                         style="border: 5px solid rgb(66, 33, 104); width: 200px; height: 200px;"
                         src="assets/images/staff/Emanuele-Papale.jpg" alt="">
-                    <div class="mb-5 mt-2 fs-4 fw-bold">Emanuele Papale <i class="fa-brands fa-linkedin"></i>
+                    <div class="mb-5 mt-2 fs-4 fw-bold">Emanuele Papale <a href="https://www.linkedin.com/in/emanuele-papale/"><i class="fa-brands fa-linkedin"></i></a>
                     </div>
                 </div>
                 <div class="col-12 col-lg-3 d-flex align-items-center flex-column">
                     <img class="object-fit-cover rounded-circle"
                         style="border: 5px solid rgb(66, 33, 104); width: 200px; height: 200px;"
                         src="assets/images/staff/Pierluigi-Perrino.png" alt="">
-                    <div class="mb-5 mt-2 fs-4 fw-bold">Pierluigi Perrino <i class="fa-brands fa-linkedin"></i></div>
+                    <div class="mb-5 mt-2 fs-4 fw-bold">Pierluigi Perrino <a href="https://www.linkedin.com/in/pierluigi-perrino/"><i class="fa-brands fa-linkedin"></i></a></div>
                 </div>
             </div>
         </div>
