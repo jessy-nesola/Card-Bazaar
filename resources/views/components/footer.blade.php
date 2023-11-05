2f5f6700da7b980a2043ecccc340ca67d09efb98
EmanuelePasquale <emanuelep.papale@gmail.com>
2023-11-05 15:50:39 +0100

    inserite icone social nel footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -12,7 +12,7 @@
 			  </li>
 			</ul>
 		  </div>
-		
+
 		{{-- LINKS --}}
 
 		<div class="col-6 col-md-2 mb-3">
@@ -26,13 +26,51 @@
 		</div>
 
 		 <div class="col-6 col-md-2 mb-3">
-			{{--<h5 class="text-light">LAVORA CON NOI</h5>
-			<ul class="nav flex-column">
-			  @if (Auth::user() && !Auth::user()->is_revisor)
-			  <li><a href="{{ route('workWithUs') }}" class="text-white-50 text-decoration-none">Lavora con noi</a>
-			  </li>
-			  @endif
-			</ul> --}}
+            <div class="social-container">
+                <ul>
+                    <li>
+                        <a class="facebook" href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <i class="fa fa-facebook" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="twitter" href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <i class="fa fa-twitter-x" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg"
+                                    width="16" height="16" fill="currentColor" class="bi bi-twitter-x"
+                                    viewBox="0 0 16 16">
+                                    <path
+                                        d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z" />
+                                </svg></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="instagram" href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="google" href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <i class="fa fa-google-plus" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
 		  </div>
 
 		<div class="col-md-5 offset-md-1 mb-3">
